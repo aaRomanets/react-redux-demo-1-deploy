b8008e631133f6cefe94c1ef85d5977488dd279c
aaRomanets <a.romanets@yandex.ru>
2023-05-20 17:49:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+//Вытаскиваем хранилище данных о пользователях
+import store from './redux/store';
 import './App.css';
+//Вытаскиваем страницу проекта
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Хранилище данных о пользователях 
+    <Provider store={store}>
+      <div className="App">
+        {/*Показываем страницу проекта */}
+        <UserContainer/>
+      </div>
+    </Provider>
   );
 }
 
